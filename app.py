d4347463c9cfab9c32e3fe171a71644f0b7facd7
Jaithra <jaithrap8@gmail.com>
2025-03-17 18:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ def predict():
 
 if __name__ == '__main__':
     logger.info("Starting Flask server...")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
